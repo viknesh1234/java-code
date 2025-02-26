54186282bf32acf63f3feb99fe5627d6b6701596
viknesh1234 <vikneshr07@gmail.com>
2025-02-26 21:48:01 +0530

    class and objects

---
+++ hotel.java
@@ -0,0 +1,8 @@
+public class hotel {
+    int coffee = 20;
+    int tea = 25;
+    public static void main(String[] args) {
+        hotel server1=new hotel();
+        System.err.println(server1.coffee);
+    }
+}
