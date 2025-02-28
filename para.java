37f4f4466ae519c53b7c1edea37824d84ff6e84b
viknesh1234 <vikneshr07@gmail.com>
2025-02-28 20:46:17 +0530

    parameter.java

---
+++ para.java
@@ -0,0 +1,20 @@
+public class para {
+    void soap(int money)
+    {
+        System.out.println(money);
+        System.out.println("soap purchased");
+    }
+    void chocolate(int money)
+    {
+         System.out.println(money);
+        System.out.println("chocolate purchased");
+    }
+    void powder(int money)
+    {
+        System.out.println(money);
+        System.out.println("powder purchased");
+    }
+
+    
+    
+}
