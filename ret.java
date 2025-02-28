e3a8c9ccc87067a58cc5dc6c4c470067732f8cb1
viknesh1234 <vikneshr07@gmail.com>
2025-02-28 21:22:48 +0530

    return.java

---
+++ ret.java
@@ -0,0 +1,12 @@
+public class ret {
+    int getsoap(int money){
+        int soap = 20;
+        int rem = 20-18;
+        return rem;
+    }
+    public static void main(String[] args) {
+        ret obj = new ret();
+        int remainder = obj.getsoap(20);
+        System.out.println(remainder);
+    }
+}
