76727b8e0ea0148dd166f1b42fc0324bf6aa5629
viknesh1234 <vikneshr07@gmail.com>
2025-03-08 21:04:30 +0530

    code.java

--- find.java
+++ find.java
@@ -1,3 +1,12 @@
+//Here is the extracted text from the image:
+//- Create a class called **"Find"** with main function  
+//- Create a function called **evenodd** with integer parameter called **int num**  
+//- Inside **Main** function get integer input from user.  
+//- Pass that input to the **evenodd** function and let the function decide whether the number is even or odd.  
+
+
+
+
 import java.util.Scanner;
 public class find {
     void evenorodd(int num){
