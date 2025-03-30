a1bdd7e4402f90146099e27c1cb751249fcd89e7
viknesh1234 <vikneshr07@gmail.com>
2025-03-30 16:47:42 +0530

    code Java

--- star.java
+++ star.java
@@ -2,7 +2,7 @@ public class star {
     public static void main(String[] args) {
         for(int star=1; star<=3; star++){
             for(int line=1; line<=star; line++){
-           // for(int line=1; line<=3; line++)
+           // for(int line=1; line<=3; line++)//
                 System.out.print("*");
             }
             System.out.println();
